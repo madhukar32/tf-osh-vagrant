a8d1f2cd46c736621f39a21d983dba96f9b83731
Madhukar Nayakbomman <madhukarnayak@hotmail.com>
2018-05-30 11:07:01 -0700

    Cleaning up the directory

--- src/multinode/scripts/enable_root_login.sh
+++ src/multinode/scripts/enable_root_login.sh
@@ -1,13 +1,1 @@
-#!/bin/bash -eux
-
-#login as root
-sudo su
-
-sudo sed -e '/PermitRootLogin/ s/^#*/#/' -i /etc/ssh/sshd_config
-sudo sed '/^#PermitRootLogin/a PermitRootLogin yes' -i /etc/ssh/sshd_config
-sudo sed -i '/^#ListenAddress 0.0.0.0/s/^#//' -i /etc/ssh/sshd_config
-sudo service ssh restart
-
-echo "root:Juniper123" | sudo chpasswd
-
-sudo iptables -F
+../scripts/enable_root_login.sh
